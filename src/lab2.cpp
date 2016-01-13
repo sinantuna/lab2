f0343a65a61be65754cb8fb067131ece78941bdd
sinantuna <sinantuna2018@u.northwestern.edu>
2016-01-13 14:49:03 -0600

    Lab 2 first 2 tasks

--- src/lab2.cpp
+++ src/lab2.cpp
@@ -4,7 +4,7 @@
 // Lab 2 programs
 //
 
-int illegalStatements() {
+//int illegalStatements() {
     /* Start here! */
     //int double = 0;
     //double char = 2.5;
@@ -13,12 +13,58 @@ int illegalStatements() {
     //int pi = 3.14;
     //short num = 1e9;
     //string mystring = 5;
-    return 0;
+    //return 0;
+//}
+
+//int main() {
+    //illegalStatements();
+    //cout << "Start with Exercise 1 (legal/illegal statements)" << endl;
+    //cout << "Then work on the exercises and code them up." << endl;
+//}
+
+void odd_or_even(int num)
+{
+    if(num%2 == 0)  //even
+        cout << num << " is even\n";
+    else  //odd
+        cout << num << " is odd\n";
 }
 
-int main() {
-    illegalStatements();
-    cout << "Start with Exercise 1 (legal/illegal statements)" << endl;
-    cout << "Then work on the exercises and code them up." << endl;
+void calc()
+{
+    string operation;
+    double first;
+    double second;
+    cin>>operation>>first>>second;
+
+    if( operation == "+"){
+        cout<<(first+second);
+    }
+
+    if( operation == "-"){
+        cout<<(first+second);
+    }
+
+    if( operation == "*"){
+        cout<<(first*second);
+    }
+
+    if( operation == "/"){
+        cout<<(first/second);
+    }
 }
+
+
+int main()  {
+
+//    odd_or_even(5);
+//    odd_or_even(8);
+//
+//    return 0;
+
+    calc();
+}
+
+
+
 
